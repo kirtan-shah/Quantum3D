5b0e871336a45d1da4154e321286dcfb6478abc1
unknown <kirtanushah@gmail.com>
2020-05-09 19:30:18 -0700

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import GameLogic from './GameLogic.js'
 const app = express()
 const port = 971
 app.use(express.static('public'))
-express.static.mime.define({'application/wasm': ['wasm']})
 
 let games = {}
 function generateId() {
